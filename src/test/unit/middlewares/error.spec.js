bdfafc8e39a907207576528d6d4cbbe58ed574fa
Sviatoslav Kushnierov <128366307+CallMeMaverick@users.noreply.github.com>
2024-07-15 21:03:48 +0300

    Unit tests for error middleware (#38)
    * test: implement unit test for handling MongoServerError
    
    Implemented test for handling MongoServerError with 11000 code
    
    * test: implement unit test for handling MongoServerError with non-11000 code
    
    Implemented test for handling MongoServerError with non-11000 code
    
    * test: implement unit test for handling ValidationError
    
    Implemented test for handling ValidationError
    
    * feat: move test logic to helpers
    
    Moved logic to handle errors into helper functions
    
    * feat: expectErrorHandling function
    
    Implemented function to assemble helper functions to avoid repetitions
    
    * test: generic error with status and code
    
    Implemented test for non-specific error with status and code

---
+++ src/test/unit/middlewares/error.spec.js
@@ -0,0 +1,141 @@
+const errorMiddleware = require("~/middlewares/error")
+const {
+    INTERNAL_SERVER_ERROR,
+    DOCUMENT_ALREADY_EXISTS,
+    MONGO_SERVER_ERROR,
+    VALIDATION_ERROR
+} = require('~/consts/errors')
+const {
+    expectErrorStatus,
+    expectJSON,
+    expectLogger
+} = require("~/test/helpers")
+const logger = require('~/logger/logger')
+const getUniqueFields = require('~/utils/getUniqueFields')
+
+jest.mock('~/logger/logger')
+jest.mock('~/utils/getUniqueFields')
+
+describe("Error middleware", () => {
+    let res, next;
+
+    const expectErrorHandling = (
+        res,
+        logger,
+        err,
+        expectedStatus,
+        expectedCode,
+        expectedMessage
+    ) => {
+        expectErrorStatus(res.status, expectedStatus);
+        expectJSON(res.json, {
+            status: expectedStatus,
+            code: expectedCode,
+            message: expectedMessage
+        });
+        expectLogger(logger.error, err);
+    };
+
+
+    beforeEach(() => {
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+        next = jest.fn()
+    })
+
+    test('Should handle MongoServerError with 11000 code', () => {
+        const err = {
+            name: "MongoServerError",
+            code: 11000,
+            message: 'duplicate key error collection: test.users index: email_1 dup key: { email: "test@example.com" }'
+        }
+
+        getUniqueFields.mockReturnValue('email')
+        errorMiddleware(err, {}, res, next);
+
+        expectErrorHandling(
+            res,
+            logger,
+            err,
+            409,
+            DOCUMENT_ALREADY_EXISTS('email').code,
+            DOCUMENT_ALREADY_EXISTS('email').message,
+        )
+        expect(getUniqueFields).toHaveBeenCalledWith(err.message)
+    })
+
+    test('Should handle MongoServerError with non-11000 code', () => {
+        const err = {
+            name: "MongoServerError",
+            code: 12345,
+            message: "some non-11000 mongo error"
+        }
+
+        errorMiddleware(err, {}, res, next)
+
+        expectErrorHandling(
+            res,
+            logger,
+            err,
+            500,
+            MONGO_SERVER_ERROR(err.message).code,
+            MONGO_SERVER_ERROR(err.message).message,
+        )
+    })
+
+    test('Should handle ValidationError', () => {
+        const err = {
+            name: "ValidationError",
+            message: "validation failed"
+        }
+
+        errorMiddleware(err, {}, res, next)
+
+        expectErrorHandling(
+            res,
+            logger,
+            err,
+            409,
+            VALIDATION_ERROR(err.message).code,
+            VALIDATION_ERROR(err.message).message,
+        )
+    })
+
+    test('Should handle generic error without status and code', () => {
+        const err = {
+            message: 'unknown error'
+        }
+
+        errorMiddleware(err, {}, res, next)
+
+        expectErrorHandling(
+            res,
+            logger,
+            err,
+            500,
+            INTERNAL_SERVER_ERROR.code,
+            err.message
+        )
+    })
+
+    test('Should handle error with status and code', () => {
+        const err = {
+            status: 400,
+            code: "BAD_REQUEST",
+            message: "bad request"
+        }
+
+        errorMiddleware(err, {}, res, next)
+
+        expectErrorHandling(
+            res,
+            logger,
+            err,
+            400,
+            err.code,
+            err.message
+        )
+    })
+})
