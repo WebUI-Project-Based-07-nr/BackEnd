f673cb9e63e1939cea59c132f9aca5f44420eabb
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-16 16:23:44 +0300

    refactor: improve readability and reduce redundancy
    Extracted repeated logic into local helpers

--- src/test/integration/services/email.spec.js
+++ src/test/integration/services/email.spec.js
@@ -15,12 +15,32 @@ jest.mock('~/configs/config');
 
 
 describe("Email service", () => {
+    const user = gmailCredentials.user
+
     const expectedSender = (user, email, templateSubject, html) => ({
         from: `Space2Study <${user}>`,
         to: email,
         subject: templateSubject,
         html
     })
+
+    const setUpTemplateList = (subject, language, template, templateSubject) => {
+        templateList[subject] = {
+            [language]: {
+                template,
+                subject: templateSubject
+            }
+        }
+    }
+
+    const mockAndRenderEmail = (html, sendMailResolved = true) => {
+        EmailTemplates.prototype.render.mockResolvedValue(html)
+        sendMailResolved ?
+            sendMail.mockResolvedValue()
+            :
+            sendMail.mockRejectedValue(new Error("Failed to send email"))
+
+    }
 
     beforeEach(() => {
         jest.clearAllMocks()
@@ -34,23 +54,15 @@ describe("Email service", () => {
 
         const template = "en/confirm-email"
         const templateSubject = "Please confirm your email"
-
-        templateList[subject] = {
-            [language]: {
-                template,
-                subject: templateSubject
-            }
-        }
-
         const html = '<h1>Greetings</h1>'
 
-        EmailTemplates.prototype.render.mockResolvedValue(html)
-        sendMail.mockResolvedValue()
+        setUpTemplateList(subject, language, template, templateSubject);
+        mockAndRenderEmail(html)
 
         await emailService.sendEmail(email, subject, language, text)
 
         expect(EmailTemplates.prototype.render).toHaveBeenCalledWith(template, text)
-        expect(sendMail).toHaveBeenCalledWith(expectedSender(gmailCredentials.user, email, templateSubject, html))
+        expect(sendMail).toHaveBeenCalledWith(expectedSender(user, email, templateSubject, html))
     })
 
     test('Should throw an error if sendEmail fails', async () => {
@@ -61,23 +73,15 @@ describe("Email service", () => {
 
         const template = "en/confirm-email"
         const templateSubject = "Please confirm your email"
-
-        templateList[subject] = {
-            [language]: {
-                template,
-                subject: templateSubject
-            }
-        }
-
         const html = '<h1>Greetings</h1>'
 
-        EmailTemplates.prototype.render.mockResolvedValue(html)
-        sendMail.mockRejectedValue(new Error("Failed to send email"))
+        setUpTemplateList(subject, language, template, templateSubject);
+        mockAndRenderEmail(html, false)
 
         await expect(emailService.sendEmail(email, subject, language, text)).rejects.toThrow("Failed to send email")
 
         expect(EmailTemplates.prototype.render).toHaveBeenCalledWith(template, text)
-        expect(sendMail).toHaveBeenCalledWith(expectedSender(gmailCredentials.user, email, templateSubject, html))
+        expect(sendMail).toHaveBeenCalledWith(expectedSender(user, email, templateSubject, html))
     })
 
     test('Should throw an error when template is not found', async () => {
@@ -103,5 +107,27 @@ describe("Email service", () => {
         expect(createError).toHaveBeenCalledWith(404, TEMPLATE_NOT_FOUND)
         expect(EmailTemplates.prototype.render).not.toHaveBeenCalled()
         expect(sendMail).not.toHaveBeenCalled()
+    })
+
+    test('Should send an email with correct template and no text provided', async () => {
+        const email = "test@example.com"
+        const subject = "EMAIL_CONFIRMATION"
+        const language = "en"
+        const text = {}
+
+        const template = 'en/confirm-email';
+        const templateSubject = 'Please confirm your email';
+        const html = '<h1>Greetings</h1>'
+
+        setUpTemplateList(subject, language, template, templateSubject);
+        mockAndRenderEmail(html)
+
+        EmailTemplates.prototype.render.mockResolvedValue(html)
+        sendMail.mockResolvedValue()
+
+        await emailService.sendEmail(email, subject, language, text)
+
+        expect(EmailTemplates.prototype.render).toHaveBeenCalledWith(template, text)
+        expect(sendMail).toHaveBeenCalledWith(expectedSender(user, email, templateSubject, html))
     })
 })
