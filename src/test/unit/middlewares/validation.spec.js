3d4320b0fd984d7023b334f8836c3ff0d71918f2
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-17 12:03:53 +0300

    test: test case for undefined body
    Implemented test case for handling undefined body

---
+++ src/test/unit/middlewares/validation.spec.js
@@ -0,0 +1,44 @@
+jest.mock('~/utils/errorsHelper', () => ({
+    createError: jest.fn((status, errorInfo) => {
+        const err = new Error(errorInfo.message)
+        err.status = status
+        err.code = errorInfo.code
+
+        return err
+    })
+}))
+
+jest.mock('~/utils/validationHelper', () => ({
+    validateRequired: jest.fn(),
+    validateFunc: {
+        required: jest.fn(),
+        type: jest.fn(),
+        length: jest.fn(),
+        regex: jest.fn(),
+        enum: jest.fn(),
+    }
+}))
+
+const validationMiddleware = require("~/middlewares/validation")
+const { BODY_IS_NOT_DEFINED } = require('~/consts/errors')
+const { validateRequired, validateFunc } = require('~/utils/validationHelper')
+const { createError } = require('~/utils/errorsHelper')
+
+describe("Validation middleware", () => {
+    let req, res, next
+
+    beforeEach(() => {
+        req = { body: {} }
+        res = {}
+        next = jest.fn()
+    })
+
+    test('Should throw BODY_IS_NOT_DEFINED with 422 status when body is not defined', () => {
+        req.body = undefined
+
+        const schema = {}
+
+        expect(() => validationMiddleware(schema)(req, res, next))
+            .toThrow(createError(422, BODY_IS_NOT_DEFINED))
+    })
+})
