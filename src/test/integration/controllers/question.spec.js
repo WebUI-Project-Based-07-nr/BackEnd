e3fdab6b9a11cada37e768e0d7c949f17e399bde
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-19 00:06:01 +0300

    test: test suite for getQuestionById
    Implemented test suite for getQuestionById func of question controller

--- src/test/integration/controllers/question.spec.js
+++ src/test/integration/controllers/question.spec.js
@@ -1,17 +1,29 @@
 const request = require('supertest')
 const express = require('express')
 const bodyParser = require('body-parser')
-const questionService = require('~/services/question')
+const {
+    getQuestions,
+    getQuestionById,
+    createQuestion,
+    deleteQuestion,
+    updateQuestion
+} = require('~/controllers/question');
+const questionService = require('~/services/question');
 const getCategoriesOptions = require('~/utils/getCategoriesOption')
 const getMatchOptions = require('~/utils/getMatchOptions')
 const getSortOptions = require('~/utils/getSortOptions')
-const { getQuestions } = require('~/controllers/question')
 const { connect, closeDatabase, clearDatabase } = require('~/test/dbHandler')
+const isEntityValid = require('~/middlewares/entityValidation')
+const asyncWrapper = require('~/middlewares/asyncWrapper')
+const Question = require('~/models/question')
 
 jest.mock('~/services/question')
 jest.mock('~/utils/getCategoriesOption')
 jest.mock('~/utils/getMatchOptions')
 jest.mock('~/utils/getSortOptions')
+jest.mock('~/models/question', () => ({
+    findById: jest.fn() // Corrected mock definition
+}));
 
 const app = express()
 app.use(bodyParser.json())
@@ -24,6 +36,10 @@ app.use((req, res, next) => {
 });
 
 app.get('/questions', getQuestions)
+app.get('/questions/:id',
+    isEntityValid( { params: [ {model: Question, idName: 'id' }] } ),
+    asyncWrapper(getQuestionById)
+)
 
 describe('Question controller', () => {
     beforeAll(async () => {
@@ -39,41 +55,41 @@ describe('Question controller', () => {
         jest.clearAllMocks()
     })
 
+    const mockQuestions = {
+        items: [
+            {
+                title: 'Question 1',
+                text: 'What is the capital of France?',
+                answers: [
+                    { text: 'Paris', isCorrect: true },
+                    { text: 'London', isCorrect: false },
+                    { text: 'Berlin', isCorrect: false },
+                    { text: 'Madrid', isCorrect: false }
+                ],
+                type: 'multiple-choice',
+                category: { _id: '1', name: 'Geography' },
+                author: { _id: 'userId1', name: 'Author 1' },
+                resourceType: 'educational'
+            },
+            {
+                title: 'Question 2',
+                text: 'What is 2 + 2?',
+                answers: [
+                    { text: '3', isCorrect: false },
+                    { text: '4', isCorrect: true },
+                    { text: '5', isCorrect: false }
+                ],
+                type: 'multiple-choice',
+                category: { _id: '2', name: 'Mathematics' },
+                author: { _id: 'userId2', name: 'Author 2' },
+                resourceType: 'educational'
+            }
+        ],
+        count: 2
+    };
+
     describe('getQuestions', () => {
         test('Should retrieve questions based on query param and user', async () => {
-            const mockQuestions = {
-                items: [
-                    {
-                        title: 'Question 1',
-                        text: 'What is the capital of France?',
-                        answers: [
-                            { text: 'Paris', isCorrect: true },
-                            { text: 'London', isCorrect: false },
-                            { text: 'Berlin', isCorrect: false },
-                            { text: 'Madrid', isCorrect: false }
-                        ],
-                        type: 'multiple-choice',
-                        category: { _id: '1', name: 'Geography' },
-                        author: { _id: 'userId1', name: 'Author 1' },
-                        resourceType: 'educational'
-                    },
-                    {
-                        title: 'Question 2',
-                        text: 'What is 2 + 2?',
-                        answers: [
-                            { text: '3', isCorrect: false },
-                            { text: '4', isCorrect: true },
-                            { text: '5', isCorrect: false }
-                        ],
-                        type: 'multiple-choice',
-                        category: { _id: '2', name: 'Mathematics' },
-                        author: { _id: 'userId2', name: 'Author 2' },
-                        resourceType: 'educational'
-                    }
-                ],
-                count: 2
-            };
-
             const mockCategoriesOptions = ['Geography', 'Mathematics'];
             const mockMatchOptions = {
                 author: 'userId',
@@ -116,6 +132,52 @@ describe('Question controller', () => {
                 0,
                 10
             )
+        })
+    })
+
+    describe('getQuestionById', () => {
+        test('Should retrieve a question by id', async () => {
+            const mockQuestion = {
+                _id: 'questionId1',
+                title: 'Question 1',
+                text: 'What is the capital of France?',
+                answers: [
+                    { text: 'Paris', isCorrect: true },
+                    { text: 'London', isCorrect: false },
+                    { text: 'Berlin', isCorrect: false },
+                    { text: 'Madrid', isCorrect: false }
+                ],
+                type: 'multiple-choice',
+                category: { _id: '1', name: 'Geography' },
+                author: { _id: 'userId1', name: 'Author 1' },
+                resourceType: 'educational'
+            };
+
+            Question.findById.mockResolvedValue(mockQuestion);
+            questionService.getQuestionById.mockResolvedValue(mockQuestion);
+
+            const res = await request(app)
+                .get('/questions/questionId1')
+                .set('user', JSON.stringify({ id: "userId" }));
+
+            expect(res.status).toBe(200);
+            expect(res.body).toEqual(mockQuestion);
+            expect(Question.findById).toHaveBeenCalledWith('questionId1');
+            expect(questionService.getQuestionById).toHaveBeenCalledWith('questionId1');
+        })
+
+        test('Should return 404 if question is not found', async () => {
+            Question.findById.mockResolvedValue(null)
+            questionService.getQuestionById.mockResolvedValue(null)
+
+            const res = await request(app)
+                .get('/question/questionId1')
+                .set('user', JSON.stringify({ id: 'userId' }))
+
+            expect(res.status).toBe(404)
+            expect(res.body).toEqual({})
+            expect(Question.findById).not.toHaveBeenCalledWith()
+            expect(questionService.getQuestionById).not.toHaveBeenCalledWith()
         })
     })
 })
