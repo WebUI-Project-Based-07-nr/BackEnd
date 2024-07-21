0a798f0cbcf8e213aa32f52058d4dce723f781af
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-21 12:03:44 +0300

    feat(auth): add BAD_ID_TOKEN error to handle invalid Google ID token

--- src/consts/errors.js
+++ src/consts/errors.js
@@ -75,6 +75,10 @@ const errors = {
     code: 'BAD_RESET_TOKEN',
     message: 'The reset token is either invalid or has expired.'
   },
+  BAD_ID_TOKEN: {
+    code: 'BAD_ID_TOKEN',
+    message: 'ID token is either invalid or has expired'
+  },
   INVALID_TOKEN_NAME: {
     code: 'INVALID_TOKEN_NAME',
     message: 'The token name you used is invalid.'
