c68d5525cc46cd6f8da64d12aa1f4eb8665dc5ff
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-24 00:13:18 +0300

    feat(email-confirmation): add confirmEmail service
    Implemented confirmEmail service to validate confirm token and set user's email to confirmed

---
+++ src/services/confirmEmail.js
@@ -0,0 +1,21 @@
+const User = require('~/models/user')
+const tokenService = require('~/services/token')
+const { createError } = require('~/utils/errorsHelper')
+const { BAD_CONFIRM_TOKEN } = require('~/consts/errors')
+
+const confirmEmail = async (confirmToken) => {
+    const decoded = tokenService.validateConfirmToken(confirmToken)
+    if (!decoded) throw createError(400, BAD_CONFIRM_TOKEN)
+
+    const user = await User.findById(decoded.id)
+    if (!user || user.isEmailConfirmed) throw createError(400, BAD_CONFIRM_TOKEN)
+
+    user.isEmailConfirmed = true
+    await user.save()
+
+    return user
+}
+
+module.exports = {
+    confirmEmail
+}
