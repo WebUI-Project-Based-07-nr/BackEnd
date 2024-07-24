9fa65a3369aa170ceebc8251eee4726a3ccf9045
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-24 11:45:26 +0300

    docs(swagger): add swagger definition for confirm-email route

--- src/routes/confirmEmail.js
+++ src/routes/confirmEmail.js
@@ -3,6 +3,25 @@ const router = require('express').Router()
 const asyncWrapper = require('~/middlewares/asyncWrapper')
 const confirmEmailController = require('~/controllers/confirmEmail')
 
-router.get('/confirm-email', asyncWrapper(confirmEmailController.confirmEmail))
+/**
+ * @swagger
+ * /auth/confirm-email:
+ *   get:
+ *     summary: Confirm user email
+ *     tags: [Auth]
+ *     parameters:
+ *       - in: query
+ *         name: confirmToken
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Token to confirm email
+ *     responses:
+ *       200:
+ *         description: Email successfully confirmed
+ *       400:
+ *         description: Invalid or expired token
+ */
+router.get('/auth/confirm-email', asyncWrapper(confirmEmailController.confirmEmail))
 
 module.exports = router
