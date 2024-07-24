366330da34ed3968618a65ddb9c88dbfc0f69aa5
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-24 13:24:47 +0300

    docs(swagger): update swagger docs

--- src/routes/location.js
+++ src/routes/location.js
@@ -3,6 +3,20 @@ const router = require('express').Router()
 const asyncWrapper = require('~/middlewares/asyncWrapper')
 const locationController = require('~/controllers/location')
 
+/**
+ * @swagger
+ * /countries:
+ *   get:
+ *     summary: Get list of countries
+ *     tags: [Location]
+ *     responses:
+ *       200:
+ *         description: List of countries retrieved successfully
+ *       400:
+ *         description: Invalid request
+ *       500:
+ *         description: Server error
+ */
 router.get('/', asyncWrapper(locationController.getCountries))
 
 module.exports = router
