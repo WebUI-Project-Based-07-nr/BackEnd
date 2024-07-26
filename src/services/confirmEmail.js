439d49e1a5412b8eb7b96d9dced51de1305d1690
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-26 10:42:46 +0300

    chore: resolve sonar issues

--- src/services/confirmEmail.js
+++ src/services/confirmEmail.js
@@ -3,12 +3,16 @@ const tokenService = require('~/services/token')
 const { createError } = require('~/utils/errorsHelper')
 const { BAD_CONFIRM_TOKEN } = require('~/consts/errors')
 
-const confirmEmail = async (confirmToken) => {
+const confirmEmail = async confirmToken => {
     const decoded = tokenService.validateConfirmToken(confirmToken)
-    if (!decoded) throw createError(400, BAD_CONFIRM_TOKEN)
+    if (!decoded) {
+        throw createError(400, BAD_CONFIRM_TOKEN)
+    }
 
     const user = await User.findById(decoded.id)
-    if (!user || user.isEmailConfirmed) throw createError(400, BAD_CONFIRM_TOKEN)
+    if (!user || user.isEmailConfirmed) {
+        throw createError(400, BAD_CONFIRM_TOKEN)
+    }
 
     user.isEmailConfirmed = true
     await user.save()
