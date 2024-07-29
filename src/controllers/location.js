90ddd8520312d6af078a0a7d1cc5f0781c6589ba
Sviatoslav Kushnierov <128366307+CallMeMaverick@users.noreply.github.com>
2024-07-29 19:30:09 +0300

    Location service (#65)
    * config(location-api-key): configured  COUNTRY_CITY_STATE_API_KEY
    
    * config(location-api): add endpoint for location api
    
    * feat(location): implement location service
    
    Implemented location service to fetch countries
    
    * feat(location): implement location controller
    
    * feat(location): update router
    
    * docs(swagger): update swagger docs
    
    * test(location-service): implement unit tests for location service
    
    * test(location-service): implement integration test for location controller
    
    * test(location-service): add coverage
    
    Covered the line that re-throws error to hit 100% coverage for service
    
    * test(location-controller): implement integration test for location controller
    
    * test(location-service): add coverage
    
    Covered the line that re-throws error to hit 100% coverage for service
    
    * chore: remove semicolons
    
    * feat(location-service): add 404 handling
    
    * feat(location-controller): add 404 handling
    
    * test(location-service): add 404 case
    
    * test(location-controller): add 404 case
    
    * feat(location-route): add /location prefix to countries endpoint
    
    Implemented /location endpoint for better API organization and reusability for another calls
    
    * docs(location): adjust swagger docs to 404
    
    * feat(location-controller): improve error handling
    
    * test(location-controller): adjust test for handling 500
    
    * chore: resolve sonar issues
    
    * refactor(tests): replace try/catch

---
+++ src/controllers/location.js
@@ -0,0 +1,24 @@
+const locationService = require('~/services/location')
+const errors = require('~/consts/errors')
+
+const getCountries = async (_req, res) => {
+    try {
+        const countries = await locationService.fetchCountries()
+        res.json(countries)
+    } catch (error) {
+        let statusCode = 500
+
+        if (error.code === errors.NOT_FOUND.code) {
+            statusCode = 404
+        } else if (error.code === errors.BAD_REQUEST.code) {
+            statusCode = 400
+        }
+
+        res.status(statusCode).json({
+            code: error.code,
+            message: error.message
+        })
+    }
+}
+
+module.exports = { getCountries }
---
+++ src/test/integration/controllers/location.spec.js
@@ -0,0 +1,66 @@
+const { serverInit, serverCleanup, stopServer } = require('~/test/setup')
+const errors = require('~/consts/errors')
+const locationService = require('~/services/location')
+
+jest.mock('~/services/location')
+
+
+describe('Location controller', () => {
+    let app, server
+
+    beforeAll(async () => {
+        ; ({ app, server } = await serverInit())
+    })
+
+    afterEach(async () => {
+        await serverCleanup()
+    })
+
+    afterAll(async () => {
+        await stopServer(server)
+    })
+
+    test('Should return countries successfully', async () => {
+        const mockData = [{ iso2: 'US', name: 'United States' }]
+        locationService.fetchCountries.mockResolvedValue(mockData)
+
+        const response = await app
+            .get('/location/countries')
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(mockData)
+    });
+
+    test('Should handle 400 error', async () => {
+        const error400 = { ...errors.BAD_REQUEST }
+
+        locationService.fetchCountries.mockRejectedValue(error400)
+
+        const response = await app.get('/location/countries')
+
+        expect(response.status).toBe(400)
+        expect(response.body).toEqual(error400)
+    })
+
+    test('Should handle 404 error', async () => {
+        const error404 = { ...errors.NOT_FOUND }
+
+        locationService.fetchCountries.mockRejectedValue(error404)
+
+        const response = await app.get('/location/countries')
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual(error404)
+    })
+
+    test('Should handle 500 error', async () => {
+        const error500 = { ...errors.INTERNAL_SERVER_ERROR }
+
+        locationService.fetchCountries.mockRejectedValue(error500)
+
+        const response = await app.get('/location/countries')
+
+        expect(response.status).toBe(500)
+        expect(response.body).toEqual(error500)
+    })
+})
