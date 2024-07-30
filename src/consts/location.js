0430bc9dbc4126937bdd3a8a3141f034b4a75ceb
«Sviatoslav» <ksvyat21@gmail.com>
2024-07-30 11:44:21 +0300

    feat(consts): endpoint for fetching cities
    Added endpoint for fetching cities by country code

--- src/consts/location.js
+++ src/consts/location.js
@@ -1,5 +1,6 @@
 const endpoints = {
-    BASE_URL: 'https://api.countrystatecity.in/v1/countries'
+    BASE_URL: 'https://api.countrystatecity.in/v1/countries',
+    CITIES_ENDPOINT: (countryCode) => `https://api.countrystatecity.in/v1/countries/${countryCode}/cities`
 }
 
 module.exports = { endpoints }
