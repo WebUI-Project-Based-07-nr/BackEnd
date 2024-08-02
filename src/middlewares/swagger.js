abf67e04947605fe28d2bb6b6684a7f84d20cd4e
«Sviatoslav» <ksvyat21@gmail.com>
2024-08-02 15:29:45 +0300

    docs(swagger): update swagger
    Updated swagger to include schemas for category and create category request

--- src/middlewares/swagger.js
+++ src/middlewares/swagger.js
@@ -36,6 +36,7 @@ const GetUsersScheme = require('~/docs/schemas/users/get-users')
 const UpdateUserByIdScheme = require('~/docs/schemas/users/update-user-by-id')
 
 const CategoryScheme = require('~/docs/schemas/category/category')
+const CreateCategoryScheme = require('~/docs/schemas/category/create-category')
 
 const options = {
   definition: {
@@ -73,7 +74,8 @@ const options = {
         ...DeleteUserByIdScheme,
         ...GetUsersScheme,
         ...UpdateUserByIdScheme,
-        ...CategoryScheme
+        ...CategoryScheme,
+        ...CreateCategoryScheme
       }
     }
   },
