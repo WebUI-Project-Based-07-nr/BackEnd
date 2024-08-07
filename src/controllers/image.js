7b5b36f7e1b39164cb20119b07f8880bda624544
«Sviatoslav» <ksvyat21@gmail.com>
2024-08-07 16:00:19 +0300

    feat(image-controller): implement image controller

---
+++ src/controllers/image.js
@@ -0,0 +1,26 @@
+const imageService = require('~/services/image')
+const User = require('~/models/user')
+
+const uploadImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send({ error: 'No file uploaded' })
+    }
+
+    const userId = req.user.id
+    const buffer = req.file.buffer
+    const mimetype = req.file.mimetype
+
+    await imageService.uploadImage(buffer, mimetype, mimetype)
+
+    await User.findByIdAndUpdate(userId, { photo: `images/${userId}` })
+
+    res.status(200).send({ message: 'File uploaded successfully' })
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+}
+
+module.exports = {
+  uploadImage
+}
