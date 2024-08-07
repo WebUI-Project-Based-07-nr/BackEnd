34966f442db2069766a0ec8ebb055095f2bbf1e5
«Sviatoslav» <ksvyat21@gmail.com>
2024-08-08 00:07:42 +0300

    feat(offer-model): offer model validation
    Implemented validation for offer model according to the task description

--- src/models/offer.js
+++ src/models/offer.js
@@ -4,43 +4,73 @@ const {
   enums: { MAIN_ROLE_ENUM, SPOKEN_LANG_ENUM, PROFICIENCY_LEVEL_ENUM, OFFER_STATUS_ENUM }
 } = require('~/consts/validation')
 const { USER, OFFER, CATEGORY, SUBJECT } = require('~/consts/models')
-const { ENUM_CAN_BE_ONE_OF } = require('~/consts/errors')
+const {
+  ENUM_CAN_BE_ONE_OF,
+  FIELD_CANNOT_BE_EMPTY,
+  VALUE_MUST_BE_ABOVE,
+  FIELD_CANNOT_BE_SHORTER,
+  FIELD_CANNOT_BE_LONGER
+} = require('~/consts/errors')
 
 const offerSchema = new Schema(
   {
     price: {
-      type: Number
+      type: Number,
+      min: [1, VALUE_MUST_BE_ABOVE('price', 1)],
+      required: [true, FIELD_CANNOT_BE_EMPTY('price')]
     },
     proficiencyLevel: {
       type: String,
       enum: {
         values: PROFICIENCY_LEVEL_ENUM,
         message: ENUM_CAN_BE_ONE_OF('proficiency level', PROFICIENCY_LEVEL_ENUM)
-      }
+      },
+      required: [true, FIELD_CANNOT_BE_EMPTY('proficiency level')]
     },
     title: {
-      type: String
+      type: String,
+      required: [true, FIELD_CANNOT_BE_EMPTY('title')],
+      minValue: [1, FIELD_CANNOT_BE_SHORTER('title', 1)],
+      maxValue: [100, FIELD_CANNOT_BE_LONGER('title', 100)],
+      validate: {
+        validator: (title) => {
+          return title.trim().length > 0
+        },
+        message: 'Title cannot contain only whitespace'
+      }
     },
     description: {
-      type: String
+      type: String,
+      required: [true, FIELD_CANNOT_BE_EMPTY('description')],
+      minValue: [1, FIELD_CANNOT_BE_SHORTER('description', 1)],
+      maxValue: [1000, FIELD_CANNOT_BE_LONGER('description', 1000)],
+      validate: {
+        validator: (description) => {
+          return description.trim().length > 0
+        },
+        message: 'Description cannot contain only whitespace'
+      }
     },
     languages: {
       type: [String],
       enum: {
         values: SPOKEN_LANG_ENUM,
         message: ENUM_CAN_BE_ONE_OF('language', SPOKEN_LANG_ENUM)
-      }
+      },
+      required: [true, FIELD_CANNOT_BE_EMPTY('languages')]
     },
     authorRole: {
       type: String,
       enum: {
         values: MAIN_ROLE_ENUM,
         message: ENUM_CAN_BE_ONE_OF('author role', MAIN_ROLE_ENUM)
-      }
+      },
+      required: [true, FIELD_CANNOT_BE_EMPTY('author role')]
     },
     author: {
       type: Schema.Types.ObjectId,
-      ref: USER
+      ref: USER,
+      required: [true, FIELD_CANNOT_BE_EMPTY('author')]
     },
     status: {
       type: String,
@@ -54,21 +84,37 @@ const offerSchema = new Schema(
       type: [
         {
           question: {
-            type: String
+            type: String,
+            required: [true, FIELD_CANNOT_BE_EMPTY('question')],
+            validate: {
+              validator: (question) => {
+                return question.trim().length > 0
+              },
+              message: 'Question cannot contain only whitespace'
+            }
           },
           answer: {
-            type: String
+            type: String,
+            required: [true, FIELD_CANNOT_BE_EMPTY('answer')],
+            validate: {
+              validator: (answer) => {
+                return answer.trim().length > 0
+              },
+              message: 'Answer cannot contain only whitespace'
+            }
           }
         }
       ]
     },
     category: {
       type: Schema.Types.ObjectId,
-      ref: CATEGORY
+      ref: CATEGORY,
+      required: [true, FIELD_CANNOT_BE_EMPTY('category')]
     },
     subject: {
       type: Schema.Types.ObjectId,
-      ref: SUBJECT
+      ref: SUBJECT,
+      required: [true, FIELD_CANNOT_BE_EMPTY('subject')]
     }
   },
   {
