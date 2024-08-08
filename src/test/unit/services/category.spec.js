70abff1f376cbeb18b5e73f2230d0066cf88fb83
«Sviatoslav» <ksvyat21@gmail.com>
2024-08-08 18:02:13 +0300

    test(category-service): test suite for subjects fetching

--- src/test/unit/services/category.spec.js
+++ src/test/unit/services/category.spec.js
@@ -1,8 +1,13 @@
 const Category = require('~/models/category')
+const Subject = require('~/models/subject')
 const categoryService = require('~/services/category')
 const { DOCUMENT_ALREADY_EXISTS } = require('~/consts/errors')
+const mongoose = require('mongoose')
+const { createError } = require('~/utils/errorsHelper')
+const { BAD_REQUEST } = require('~/consts/errors')
 
 jest.mock('~/models/category')
+jest.mock('~/models/subject')
 
 describe('Category service', () => {
     afterEach(() => {
@@ -92,6 +97,42 @@ describe('Category service', () => {
 
             await expect(categoryService.createCategory({ ...incompleteData }))
                 .rejects.toThrow('The request could not be processed due to invalid or missing parameters.')
+        })
+    })
+
+    describe('Fetching subject by category ID', () => {
+        test('Should return subject for valid category id', async () => {
+            const categoryId = new mongoose.Types.ObjectId()
+            const mockSubjects = [{ name: 'Math' }, { name: 'Computer Science' }]
+
+            Subject.find.mockReturnValue({
+                select: jest.fn().mockResolvedValue(mockSubjects)
+            });
+
+            const result = await categoryService.getSubjectNamesById(categoryId)
+
+            expect(Subject.find).toHaveBeenCalledWith({ categoryId })
+            expect(result).toEqual(['Math', 'Computer Science'])
+        })
+
+        test('Should return an empty array if no subjects are found', async () => {
+            const categoryId = new mongoose.Types.ObjectId()
+
+            Subject.find.mockReturnValue({
+                select: jest.fn().mockResolvedValue([])
+            });
+
+            const result = await categoryService.getSubjectNamesById(categoryId)
+
+            expect(Subject.find).toHaveBeenCalledWith({ categoryId })
+            expect(result).toEqual([])
+        })
+
+        test('Should throw BAD_REQUEST error if no id is provided', async () => {
+            const error = createError(400, BAD_REQUEST)
+            await expect(categoryService.getSubjectNamesById())
+                .rejects
+                .toEqual(error)
         })
     })
 })
