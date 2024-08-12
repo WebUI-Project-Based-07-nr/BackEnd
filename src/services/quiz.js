5e13c327fd9d288e8c04b6a4d2a4cb197cf3d989
«Sviatoslav» <ksvyat21@gmail.com>
2024-08-12 18:45:47 +0300

    feat(quiz-service): getQuizById method

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -3,6 +3,10 @@ const Quiz = require('~/models/quiz')
 const quizService = {
   getQuizzes: async () => {
     return await Quiz.find()
+  },
+
+  getQuizById: async (id) => {
+    return await Quiz.findById(id).lean().exec()
   }
 }
 
